2bcfdc0845de04776c308e8a8233bdf250e28f42
Moshe Zada <zada.moshe@gmail.com>
2019-04-20 14:14:32 +0300

    Add init indicator

--- client/src/components/ShardsGrid/Cells/ClusterCell.vue
+++ client/src/components/ShardsGrid/Cells/ClusterCell.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="text-align: left">
-    <div v-if="cluster.relocatingShards !== 0">
+    <div v-if="cluster.relocatingShards && cluster.relocatingShards !== 0">
       <v-progress-circular
         indeterminate
         color="white"
@@ -8,6 +8,15 @@
         width="1"
       ></v-progress-circular>
       {{ cluster.relocatingShards }} relocating shards
+    </div>
+    <div v-if="cluster.initializingShards && cluster.initializingShards !== 0">
+      <v-progress-circular
+        indeterminate
+        color="white"
+        size="8"
+        width="1"
+      ></v-progress-circular>
+      {{ cluster.initializingShards }} initializing shards
     </div>
   </div>
 </template>
