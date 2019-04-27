310f9e0cf3d9b729eb295181af79eb937838767d
Moshe Zada <zada.moshe@gmail.com>
2019-04-27 18:40:04 +0300

    Handle terminal exceptions

--- server/blueprints/terminal.py
+++ server/blueprints/terminal.py
@@ -1,3 +1,4 @@
+from elasticsearch import TransportError
 from sanic.response import json
 from sanic import Blueprint
 
@@ -6,12 +7,20 @@ from connections import get_client
 terminal_bp = Blueprint('terminal')
 
 
+def format_es_exception(e: TransportError):
+    return json({"status_code": e.status_code,
+                 "error": e.error,
+                 "info": e.info})
+
+
 @terminal_bp.route('/query', methods=['POST'])
 async def close_index(request):
     client = get_client()
     body = request.json['body']
     method = request.json['method']
     url = request.json['url']
-    resp = await client.transport.perform_request(method, url, body=body)
+    try:
+        resp = await client.transport.perform_request(method, url, body=body)
+    except TransportError as e:
+        return format_es_exception(e)
     return json(resp)
-
