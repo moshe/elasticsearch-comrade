e5791a7db7ffde90f3c63af6e262994a20bcd6e2
Moshe Zada <zada.moshe@gmail.com>
2019-04-27 20:01:48 +0300

    Add Allocation button and settings and server endpoint

---
+++ client/src/components/ShardsGrid/AllocationButton.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-menu offset-y>
+    <template v-slot:activator="{ on }">
+      <v-btn small color="red" v-on="on">
+        <v-icon size="12px" dark class="mr-1">lock</v-icon>
+        Allocation
+      </v-btn>
+    </template>
+    <v-list dense two-line>
+      <div v-for="(option, i) in options" :key="option.name">
+        <v-list-tile
+          :disabled="option.name === current"
+          @click="request(option)"
+        >
+          <v-list-tile-content>
+            <v-list-tile-title>{{ option.name }}</v-list-tile-title>
+            <v-list-tile-sub-title>
+              {{ option.text }}
+            </v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider v-if="i !== options.length - 1" />
+      </div>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: "AllocationButton",
+  data() {
+    return {
+      current: "all",
+      options: [
+        {
+          name: "all",
+          text: "(default) Allows shard allocation for all kinds of shards"
+        },
+        {
+          name: "primaries",
+          text: "Allows shard allocation only for primary shards"
+        },
+        {
+          name: "new_primaries",
+          text:
+            "Allows shard allocation only for primary shards for new indices"
+        },
+        {
+          name: "none",
+          text: "No shard allocations of any kind are allowed for any indices"
+        }
+      ]
+    };
+  },
+  methods: {
+    request(option) {
+      console.log(option);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.v-btn--small {
+  height: 18px;
+  padding: 3px 7px;
+  font-size: 9px !important;
+  margin: 0;
+}
+.v-btn {
+  min-width: 50px;
+}
+</style>
---
+++ server/blueprints/cluster.py
@@ -0,0 +1,29 @@
+from sanic.response import json
+from sanic import Blueprint
+
+from connections import get_client
+
+cluster_bp = Blueprint('cluster')
+
+
+@cluster_bp.route('/reroute_shards', methods=['POST'])
+async def reroute_shard(request):
+    client = get_client()
+    node = request.json['node']
+    shards = request.json['shards']
+    await client.cluster.reroute(
+        body={"commands": [
+            {"move": {"index": shard['index'], "shard": shard['id'], "from_node": shard['nodeName'], "to_node": node}}
+            for shard in shards
+        ]}
+    )
+    return json({"status": "ok"})
+
+
+@cluster_bp.route('/allocation/<operation>', methods=['POST'])
+async def reroute_shard(request, operation):
+    assert operation in {"all", "primaries", "new_primaries", "none"}
+    client = get_client()
+    response = await client.cluster.put_settings(body={"transient": {"cluster.routing.allocation.enable": operation}})
+    assert response.get('acknowledged') is True
+    return json({"status": "ok"})
