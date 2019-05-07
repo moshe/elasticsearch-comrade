dbcaca434e6454a0b66e6c6c7039e3439d28f133
Moshe Zada <zada.moshe@gmail.com>
2019-05-07 13:37:28 +0300

    Merge manage aliases with create aliases

--- client/src/router.js
+++ client/src/router.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./Views/Home.vue";
 import TerminalView from "./Views/TerminalView.vue";
-import CreateAliasesView from "./Views/Aliases/CreateAliasesView.vue";
+import CreateAliasesView from "./Views/AliasesView.vue";
 
 Vue.use(Router);
 
