738a6c186a029ce675bb84cc2bc86edcddb254fd
Moshe Zada <zada.moshe@gmail.com>
2019-06-26 22:51:30 +0300

    Change primary color

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,7 +1,11 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
+import colors from "vuetify/es5/util/colors";
 
 Vue.use(Vuetify, {
-  iconfont: "md"
+  iconfont: "md",
+  theme: {
+    primary: colors.green.accent1
+  }
 });
