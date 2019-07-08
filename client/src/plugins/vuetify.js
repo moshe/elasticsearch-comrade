37c4ecc18e01e4981e1d551bd8acf0c59c7a701b
Moshe Zada <zada.moshe@gmail.com>
2019-07-08 16:35:56 +0300

    Adjust colors

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,11 +1,6 @@
 import Vue from "vue";
+import colors from "vuetify/es5/util/colors";
 import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
-import colors from "vuetify/es5/util/colors";
 
-Vue.use(Vuetify, {
-  iconfont: "md",
-  theme: {
-    primary2: colors.green.accent1
-  }
-});
+Vue.use(Vuetify, { theme: { primary: colors.lightBlue.darken1 } });
