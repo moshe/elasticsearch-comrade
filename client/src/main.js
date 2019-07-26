ebf7f4a0dcdd399e2a93091918dd40b635564f77
Moshe Zada <zada.moshe@gmail.com>
2019-07-26 12:21:40 +0300

    Install icons

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,15 @@
+import "@mdi/font/css/materialdesignicons.css";
 import Vue from "vue";
 import Router from "vue-router";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import colors from "vuetify/es5/util/colors";
 // import "./plugins/vuetify";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "./style/smallTable.css";
-
 Vue.use(Vuetify);
 
 Vue.use(Router);
@@ -17,6 +18,21 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  vuetify: new Vuetify({ theme: { dark: true } }),
+  vuetify: new Vuetify({
+    theme: {
+      dark: true,
+      options: { customProperties: true },
+      themes: {
+        light: {
+          blue1: colors.lightBlue.darken1,
+          blue2: colors.lightBlue.lighten1
+        },
+        dark: {
+          blue1: colors.lightBlue.darken1,
+          blue2: colors.lightBlue.darken2
+        }
+      }
+    }
+  }),
   render: h => h(App)
 }).$mount("#app");
