61c6e39bdea90ba7c94bda55ea7270e77137f3e8
Moshe Zada <zada.moshe@gmail.com>
2019-08-12 11:23:43 +0300

    polyfill fetch

--- e2e/cypress/integration/examples/misc.spec.js
+++ e2e/cypress/integration/examples/misc.spec.js
@@ -2,33 +2,50 @@
 
 context('Misc', () => {
   beforeEach(() => {
+    cy.server()
+    cy.request({
+      method: 'POST',
+      url: '/api/v1/rest/query', body: {path: '/testindex',method: 'DELETE', body:{}},
+      headers: { 'x-elastic-cluster': 'es-primary' }
+    })
+    cy.route('/api/v1/clients').as('getClients')
+      cy.request({
+        method: 'POST',
+        url: '/api/v1/rest/query', body: {
+          body: {"settings": {"index": {"number_of_shards": 12,"number_of_replicas": 0}}},
+          path: '/testindex',
+          method: 'PUT'
+        },
+        headers: {'x-elastic-cluster': 'es-primary'}})
+    cy.route('/api/v1/cluster/info/*').as('clusterInfo')
+    cy.route('/api/v1/views/shards_grid').as('shardsGrid')
+    cy.route('/api/v1/index/testindex/dynamicSettings').as('indexSettings')
+    cy.route('POST', '/api/v1/index/testindex/settings').as('postSettings')
     cy.visit('/')
+    cy.wait(['@getClients', '@clusterInfo'])
+    cy.get('.cluster[data-cluster="es-primary"]').click()
+    cy.wait('@shardsGrid')
   })
-
-  context('Cypress.Screenshot', function () {
+  describe('Change settings', () => {
+    it('Should add replicas', () => {
+      cy.get('.index-name[data-index-name="testindex"]').click()
+      cy.get('[data-tile="edit-settings"]').click()
+      cy.wait(['@indexSettings', '@shardsGrid'])
+      cy.get('[data-tab="dynamic-index settings"]').click()
+      cy.get('[data-input="index.number_of_replicas"]').clear().type(3).blur()
+      cy.get('[data-btn="save"]').eq(1).click()
+      cy.wait('@postSettings')
+      cy.get('[data-input="index.number_of_replicas"]').clear().type(0).blur()
+      cy.get('[data-btn="save"]').eq(1).click()
+      cy.wait('@postSettings')
+    })
+  })
+  describe('Relocate shard', () => {
     it('cy.screenshot() - take a screenshot', () => {
-      // https://on.cypress.io/screenshot
-      cy.screenshot('my-image')
+      cy.get('.shard-square[data-node="node-1"]').eq(1).click()
+      cy.get('[data-cy="toggle-relocation"]').click()
+      cy.get('.node-name[data-node="node-2"]').click()
+      cy.get('[data-cy="relocate-to-node"]').click()
     })
-
-    it('Cypress.Screenshot.defaults() - change default config of screenshots', function () {
-      Cypress.Screenshot.defaults({
-        blackout: ['.foo'],
-        capture: 'viewport',
-        clip: { x: 0, y: 0, width: 200, height: 200 },
-        scale: false,
-        disableTimersAndAnimations: true,
-        screenshotOnRunFailure: true,
-        beforeScreenshot () { },
-        afterScreenshot () { },
-      })
-    })
-  })
-
-  it('cy.wrap() - wrap an object', () => {
-    // https://on.cypress.io/wrap
-    cy.wrap({ foo: 'bar' })
-      .should('have.property', 'foo')
-      .and('include', 'bar')
   })
 })
