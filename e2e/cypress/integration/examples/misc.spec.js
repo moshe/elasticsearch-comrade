7cde990feb23740e6c935c1b5fb8a8697ad1f725
Moshe Zada <zada.moshe@gmail.com>
2019-08-14 10:50:03 +0300

    better tests

--- e2e/cypress/integration/examples/misc.spec.js
+++ e2e/cypress/integration/examples/misc.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-function createIndex(name, shards=12, replicas=0) {
+function createIndex(name, shards=12, replicas=1) {
   cy.request({
     method: 'POST',
     url: '/api/v1/rest/query', body: {
@@ -22,11 +22,9 @@ function deleteIndex(name){
 context('Misc', () => {
   beforeEach(() => {
     cy.server()
-    deleteIndex('testIndex')
+    deleteIndex('test*')
     deleteIndex('.special-index')
     cy.route('/api/v1/clients').as('getClients')
-    createIndex('testindex')
-    createIndex('.special-index')
     cy.route('/api/v1/cluster/info/*').as('clusterInfo')
     cy.route('/api/v1/views/shards_grid').as('shardsGrid')
     cy.route('/api/v1/index/testindex/dynamicSettings').as('indexSettings')
@@ -38,31 +36,34 @@ context('Misc', () => {
   })
   describe('Filters', () => {
     it('Should filter special indices', () => {
+      createIndex('.special-index', 12, 0)
       cy.get('[data-index-name=".special-index"]').should('exist')
       cy.get('[data-cy="show-hidden"]').click({force: true})
       cy.get('[data-index-name=".special-index"]').should('not.exist')
+      deleteIndex('.special-index')
     })
   })
   describe('Change settings', () => {
     it('Should add replicas', () => {
+      createIndex('testindex', 12, 0)
       cy.get('.index-name[data-index-name="testindex"]').click()
       cy.get('[data-tile="edit-settings"]').click()
       cy.wait(['@indexSettings', '@shardsGrid'])
       cy.get('[data-tab="dynamic-index settings"]').click()
-      cy.get('[data-input="index.number_of_replicas"]').clear().type(3).blur()
+      cy.get('[data-input="index.number_of_replicas"]').clear().type(1).blur()
       cy.get('[data-btn="save"]:not(.v-btn--disabled)').click()
       cy.wait('@postSettings')
-      cy.get('[data-input="index.number_of_replicas"]').clear().type(0).blur()
-      cy.get('[data-btn="save"]:not(.v-btn--disabled)').click()
-      cy.wait('@postSettings')
+      deleteIndex('testIndex')
     })
   })
   describe('Relocate shard', () => {
     it('cy.screenshot() - take a screenshot', () => {
-      cy.get('.shard-square[data-node="node-1"]').eq(1).click()
+      createIndex('testindex2', 12, 0)
+      cy.get('.shard-square[data-node="node-1"][data-index="testindex2"]').eq(1).click()
       cy.get('[data-cy="toggle-relocation"]').click()
       cy.get('.node-name[data-node="node-2"]').click()
       cy.get('[data-cy="relocate-to-node"]').click()
+      deleteIndex('testindex2')
     })
   })
 })
