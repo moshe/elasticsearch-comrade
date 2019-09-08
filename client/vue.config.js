1e13b5f51bcc2b99a35b0d4c5618137c596b0543
Moshe Zada <zada.moshe@gmail.com>
2019-09-08 09:29:16 +0300

    Lint build files

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/static/' : '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/static/" : "/",
   productionSourceMap: false,
   lintOnSave: false,
   devServer: {
